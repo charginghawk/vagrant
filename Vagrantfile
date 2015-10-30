461e0b4955429082aec850a4e58fc8ca5f0bc4e2
Thomas M. Charging Hawk <TChargingHawk@genuineinteractive.com>
2015-10-30 10:39:36 -0400

    Initial commit of my Vagrant setup

---
+++ Vagrantfile
@@ -0,0 +1,26 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure(2) do |config|
+
+# Sets the IP where you can access the site
+# Change the IP to unique value (usually by incrementing by 1)
+  config.vm.network "private_network", ip: "192.168.50.1"
+
+# Choose what OS your VM will run, see: atlas.hashicorp.com/search
+# vivid for D8, trusty for D7
+#  config.vm.box = "ubuntu/trusty64"
+  config.vm.box = "ubuntu/vivid64"
+
+# Provisioning shell script
+  config.vm.provision :shell, path: "bootstrap.sh"
+
+# Syncs the the current directory with /vagrant on the guest machine, using nfs
+  config.vm.synced_folder ".", "/vagrant", :nfs => true
+
+# Gives the VM 2GB of RAM (which is too much, but I like to have breathing room)
+  config.vm.provider :virtualbox do |vb|
+    vb.customize ["modifyvm", :id, "--memory", "2048"]
+  end
+
+end
---
+++ bootstrap.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y apache2
+apt-get install -y curl
+apt-get install -y git
+apt-get install -y unzip
+apt-get install -y php5
+apt-get install -y php5-mysql
+apt-get install -y php5-gd
+apt-get install -y php5-curl
+apt-get install -y php5-xdebug
+apt-get install -y nfs-common
+apt-get install -y nfs-kernel-server
+# mysql username is root, no password
+DEBIAN_PRIORITY=critical apt-get install -y mysql-server
+
+# Install drush
+if ! [ -x "$(command -v composer)" ]; then
+    sudo curl -sS https://getcomposer.org/installer | php;
+    ln -s /home/vagrant/composer.phar /usr/local/bin/composer;
+fi
+if ! [ -x "$(command -v drush)" ]; then
+# Swap drush versions as appropriate
+#    /home/vagrant/composer.phar global require drush/drush:7.*;
+    /home/vagrant/composer.phar global require drush/drush:dev-master;
+    mv /root/.composer /home/vagrant/;
+    ln -s /home/vagrant/.composer/vendor/drush/drush/drush /usr/local/bin/drush;
+fi
+
+# Create symlink from /vagrant/docroot to /var/www/html (assuming site code is in 'docroot' directory)
+if ! [ -L /var/www ]; then
+  rm -rf /var/www/html
+  ln -fs /vagrant/docroot /var/www/html
+fi
+
+# Set server config
+sed -i -- 's/AllowOverride None/AllowOverride all/g' /etc/apache2/apache2.conf
+sed -i -- 's/128M/512M/g' /etc/php5/apache2/php.ini
+echo 'xdebug.remote_enable = on' >> /etc/php5/mods-available/xdebug.ini
+echo 'xdebug.remote_connect_back = on' >> /etc/php5/mods-available/xdebug.ini
+echo 'xdebug.max_nesting_level=256' >> /etc/php5/mods-available/xdebug.ini
+a2enmod rewrite
+service apache2 restart
+
+# Install drupal console
+curl -LSs http://drupalconsole.com/installer | php
+mv console.phar /usr/local/bin/drupal
+
